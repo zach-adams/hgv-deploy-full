0e01e110e6e5c174e2a06a6c3c59ee19d71e399d
Zach <zach.adams383@gmail.com>
2015-01-15 12:50:24 -0700

    Initial commit

---
+++ roles/wordpress/templates/wp/local-config.php
@@ -0,0 +1,35 @@
+<?php
+// ** MySQL settings ** //
+/** The name of the database for WordPress */
+define('DB_NAME', '{{ wp_db_name }}');
+
+/** MySQL database username */
+define('DB_USER', '{{ wp_db_user }}');
+
+/** MySQL database password */
+define('DB_PASSWORD', '{{ wp_db_password }}');
+
+/** MySQL hostname */
+define('DB_HOST', 'localhost');
+
+/** Database Charset to use in creating database tables. */
+define('DB_CHARSET', 'utf8');
+
+/** The Database Collate type. Don't change this if in doubt. */
+define('DB_COLLATE', '');
+
+global $memecached_servers;
+
+$memcached_servers = array(
+    'default' => array(
+        '127.0.0.1:11211'
+    )
+);
+
+if( isset($_SERVER['HTTP_HOST']) && 'cache.' === substr( $_SERVER['HTTP_HOST'], 0, 6) ){
+    define('WP_SITEURL', 'http://cache.{{ host }}');
+    define('WP_HOME', 'http://cache.{{ host }}');
+    define('WP_CACHE_KEY_SALT', 'cache_{{ wp_db_name }}_1');
+}else{
+    define('WP_CACHE_KEY_SALT', '{{ wp_db_name }}_1');
+}
